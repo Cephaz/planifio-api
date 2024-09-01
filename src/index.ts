b15de4349350250205fc9b2f4940011fbe0c3a9b
Cephaz <pierrezhoou@gmail.com>
2024-09-01 18:13:05 +0200

    init: Syntax rules
    - Eslint
    - Prettier
    - Apply settings to Vscode

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, { Express } from "express";
+import express, { Express } from 'express';
 
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
